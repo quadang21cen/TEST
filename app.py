975f4b01cf95c44cb08606c8c79aaaeea75024f7
Quach Dang <85343056+quadang21cen@users.noreply.github.com>
2023-04-17 01:59:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 print("HELLO WORLD")
 
 
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/ping",methods=['POST','GET'])
-# def hello_world():
-#     return {'status': 'good!'}
-
-
-# @app.route("/",methods=['POST','GET'])
-# def newfun():
-#     return {'status': 'home!'}
+@app.route("/ping",methods=['POST','GET'])
+def hello_world():
+    return {'status': 'good!'}
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True,host='0.0.0.0',port=8080)
+@app.route("/",methods=['POST','GET'])
+def newfun():
+    return {'status': 'home!'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8080)
