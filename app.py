a9fdc2de4616fd423a2c0f323c0f372f156c2a9e
quadang21cen <quadang21cen@gmail.com>
2023-04-16 15:25:41 +0700

    new

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/ping")
+@app.route("/ping",methods=['POST','GET'])
 def hello_world():
     return {'status': 'good!'}
+
+
+@app.route("/",methods=['POST','GET'])
+def newfun():
+    return {'status': 'home!'}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
