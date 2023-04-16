4fefd178c4d8ea20a6d28c1b55d422a051fab9ba
quadang21cen <quadang21cen@gmail.com>
2023-04-17 00:50:28 +0700

    new

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/ping",methods=['POST','GET'])
-def hello_world():
-    return {'status': 'good!'}
+print("HELLO WORLD")
 
 
-@app.route("/",methods=['POST','GET'])
-def newfun():
-    return {'status': 'home!'}
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/ping",methods=['POST','GET'])
+# def hello_world():
+#     return {'status': 'good!'}
 
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
+# @app.route("/",methods=['POST','GET'])
+# def newfun():
+#     return {'status': 'home!'}
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0',port=8080)
